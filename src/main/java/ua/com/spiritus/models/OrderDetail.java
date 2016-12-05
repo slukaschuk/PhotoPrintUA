7e14cc998a9315c79a8be87bc5c6ac12760a8185
Sergey Lukaschuk <sergey.lukaschuk@gmail.com>
2016-12-06 00:20:58 +0200

    described PhotoFormat Entity

--- src/main/java/ua/com/spiritus/models/OrderDetail.java
+++ src/main/java/ua/com/spiritus/models/OrderDetail.java
@@ -1,7 +1,5 @@
 package ua.com.spiritus.models;
 
-/**
- * Created by Spirit on 05.12.2016.
- */
+
 public class OrderDetail {
 }
--- src/main/java/ua/com/spiritus/models/PhotoFormat.java
+++ src/main/java/ua/com/spiritus/models/PhotoFormat.java
@@ -1,7 +1,63 @@
 package ua.com.spiritus.models;
 
-/**
- * Created by Spirit on 05.12.2016.
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "photoformat")
 public class PhotoFormat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "photoformatid")
+    private Integer photoFormatId;
+    @Column(name = "format")
+    String format;
+
+    public PhotoFormat() {
+    }
+
+    public PhotoFormat(String format) {
+        this.format = format;
+    }
+
+    public Integer getPhotoFormatId() {
+        return photoFormatId;
+    }
+
+    public void setPhotoFormatId(Integer photoFormatId) {
+        this.photoFormatId = photoFormatId;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PhotoFormat that = (PhotoFormat) o;
+
+        if (!photoFormatId.equals(that.photoFormatId)) return false;
+        return format.equals(that.format);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = photoFormatId.hashCode();
+        result = 31 * result + format.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "PhotoFormat{" +
+                "format='" + format + '\'' +
+                '}';
+    }
 }
