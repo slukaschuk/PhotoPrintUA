07713ae418cdc0fbe9df7d5ff7ab9ca848cffc65
Sergey Lukaschuk <sergey.lukaschuk@gmail.com>
2016-12-08 12:49:32 +0200

    repair project

--- src/main/java/ua/com/spiritus/PhotoPrintUaApplication.java
+++ src/main/java/ua/com/spiritus/PhotoPrintUaApplication.java
@@ -1,8 +1,7 @@
-package PhotoPrint;
+package ua.com.spiritus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class PhotoPrintUaApplication {
